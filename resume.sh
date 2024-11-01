6c47fa5b2233287618f7198260854cf10092eac0
Namuru <dev.namuru@gmail.com>
2024-11-01 17:43:41 +0500

    Update resume.sh

--- resume.sh
+++ resume.sh
@@ -11,7 +11,7 @@ echo -e "     -##*:                      :*#*-               Резюме${RESET
 echo -e "  :#*==-=+#+.                .**+====**.            ====================${RESET}"
 echo -e " .:        .:               .:.   :.   :.           ФИ: ${RESET}${YELLOW}Namuru Iwatani${RESET}"
 echo -e "                                 ${BLOOD_RED}:*:${RESET}                Навыки: ${RESET}${CYAN}JavaScript, React, PHP, Astro, NodeJS, TypeScript${RESET}"
-echo -e "                                 ${BLOOD_RED}-%-${RESET}                Кто Я?: ${RESET}${CYAN}Software Engineer${RESET}"
+echo -e "                                 ${BLOOD_RED}-%-${RESET}                Кто Я?: ${RESET}${CYAN}Software Engineer${RESET}" 
 echo -e "                                 ${BLOOD_RED}-%-${RESET}                Хобби: ${RESET}${CYAN}Обучаюсь игре на фортепиано${RESET}"
 echo -e "                                 ${BLOOD_RED}-%-${RESET}                Контакты: ${RESET}${CYAN}dev.namuru@gmail.com${RESET}"
 echo -e "                                 ${BLOOD_RED}-%-${RESET}                Портфолио: ${RESET}${CYAN}в разработке${RESET}"
@@ -32,4 +32,3 @@ echo
 echo -e "                        ${BLOOD_RED}       ╔═══════════════════════════════════╗${RESET}"
 echo -e "                        ${BLOOD_RED}       ║        Спасибо за просмотр!       ║${RESET}"
 echo -e "                        ${BLOOD_RED}       ╚═══════════════════════════════════╝${RESET}"
-
