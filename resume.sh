c292e3653e075182e3dc87749801546e12f0d7a7
Namuru <dev.namuru@gmail.com>
2024-11-01 17:26:49 +0500

    resume

---
+++ resume.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+BLUE='\033[1;34m'
+BLOOD_RED='\033[1;31m'
+RESET='\033[0m'
+YELLOW='\033[1;33m'
+CYAN='\033[1;36m'
+
+echo
+echo -e "     -##*:                      :*#*-               Резюме${RESET}"
+echo -e "  :#*==-=+#+.                .**+====**.            ====================${RESET}"
+echo -e " .:        .:               .:.   :.   :.           ФИ: ${RESET}${YELLOW}Namuru Iwatani${RESET}"
+echo -e "                                 ${BLOOD_RED}:*:${RESET}                Навыки: ${RESET}${CYAN}JavaScript, React, PHP, Astro, NodeJS, TypeScript${RESET}"
+echo -e "                                 ${BLOOD_RED}-%-${RESET}                Кто ты?: ${RESET}${CYAN}Software Engineer${RESET}"
+echo -e "                                 ${BLOOD_RED}-%-${RESET}                Хобби: ${RESET}${CYAN}Обучаюсь игре на фортепиано${RESET}"
+echo -e "                                 ${BLOOD_RED}-%-${RESET}                Контакты: ${RESET}${CYAN}dev.namuru@gmail.com${RESET}"
+echo -e "                                 ${BLOOD_RED}-%-${RESET}                Портфолио: ${RESET}${CYAN}в разработке${RESET}"
+echo -e "                                  ${BLOOD_RED}.${RESET}"
+echo
+echo -e "         .:                        :."
+echo -e "          **:                  .**.  "
+echo -e "            +@@+.             =@@+   "
+echo -e "             :#@@@@@@@%%@@@@@@@%=    "
+echo -e "                :%@@@@@@@@@@%=       "
+echo -e "                   .=@@@@*.          "
+echo -e "                       .             "
+echo
+echo -e "                     ${BLUE}---------------------${RESET} ${BLOOD_RED}Let's Connect!${RESET} ${BLUE}---------------------${RESET}"
+echo
+echo -e "           ${CYAN}\"Словно театральная пьеса, код требует страсти, исполнения и немного безумия.\"${RESET}"
+echo
+echo -e "                        ${BLOOD_RED}       ╔═══════════════════════════════════╗${RESET}"
+echo -e "                        ${BLOOD_RED}       ║        Спасибо за просмотр!       ║${RESET}"
+echo -e "                        ${BLOOD_RED}       ╚═══════════════════════════════════╝${RESET}"
